cae6241f8b5e98f75f2c1e40232530d0744832a6
Aman <aman.bitmesra@gmail.com>
2021-06-16 00:16:34 +0200

    fix not importing db error

--- app.py
+++ app.py
@@ -7,17 +7,12 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
-from db import db
-
 app=Flask(__name__)
 app.secret_key='aman'
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///data.db'
 app.config['SQLAlCHEMY_TRACK_MODIFICATIONS']=False
 api= Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 jwt=JWT(app, authenticate, identity)
 
@@ -30,6 +25,5 @@ api.add_resource(UserRegister, '/register')
 
 
 if __name__=='__main__':
-    db.init_app(app)
     app.run(port=5000, debug=True)
 
